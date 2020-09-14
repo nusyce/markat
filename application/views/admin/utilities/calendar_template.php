e7da1c76543fff81295ef294e91a0525cbefa7e7
Amogh Bhatnagar <amogh.bhatnagar53@gmail.com>
2020-09-14 16:12:45 +0530

    Update commit

--- application/views/admin/utilities/calendar_template.php
+++ application/views/admin/utilities/calendar_template.php
@@ -11,12 +11,13 @@
             </div>
             <?php echo form_open('admin/utilities/calendar', array('id' => 'calendar-event-form')); ?>
             <div class="modal-body">
+
                 <div class="row">
                     <div class="col-md-12">
                         <?php echo render_input('title', 'utility_calendar_new_event_placeholder'); ?>
                         <?php echo render_textarea('description', 'event_description', '', array('rows' => 5)); ?>
                     </div>
-
+                    
                     <div class="col-md-6">
                         <?php echo render_select('user[]', $staffs, array('staffid', array('firstname', 'lastname')), 'Mitabeiter', '', array('required' => true, 'multiple' => true)); ?>
 
