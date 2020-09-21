829fc83602019098e3ad5f4058cf9abd4947ceed
geraud18 <taffogeraud@gamail.com>
2020-09-21 14:30:35 +0200

    PROJECT ORIGININAL TRANSLATE DES TABLES staff,aq et rb Terminer

--- application/views/admin/projects/project_aq.php
+++ application/views/admin/projects/project_aq.php
@@ -2,19 +2,19 @@
 $table_data = array(
     '<span class="hide"> - </span><div class="checkbox mass_select_all_wrap"><input type="checkbox" id="mass_select_all" data-to-table="wohnungen"><label></label></div>',
     'ID',
-    'Kalender',
-    'Kalender',
-    'Straße',
-    'Nr',
-    'Etage',
-    'Flügel',
-    'Wohnungs-Nr.',
-    'Zimmer',
-    'Schlafplätze',
-    'Möbliert' ,
-    'Inventar' ,
-    'Projekt' ,
-    'Aktiv'
+    get_transl_field('tsl_wohnungen', 'kalender','Kalender'),
+    get_transl_field('tsl_wohnungen', 'kalender','Kalender'),
+    get_transl_field('tsl_wohnungen', 'strabe','Straße'),
+    get_transl_field('tsl_wohnungen', 'nr','Nr'),
+    get_transl_field('tsl_wohnungen', 'etage','Etage'),
+    get_transl_field('tsl_wohnungen', 'flugel','Flügel'),
+    get_transl_field('tsl_wohnungen', 'wohnungs_nr','Wohnungs-Nr'),
+    get_transl_field('tsl_wohnungen', 'zimmer','Zimmer'),
+    get_transl_field('tsl_wohnungen', 'schlafplatze','Schlafplätze'),
+    get_transl_field('tsl_wohnungen', 'mobliert','Möbliert'),
+    get_transl_field('tsl_wohnungen', 'inventar','Inventar') ,
+    get_transl_field('tsl_wohnungen', 'projekt','Projekt') ,
+    get_transl_field('tsl_wohnungen', 'aktiv','Aktiv')
 );
 
 $project = ($project->id ? $project->id : -1);
--- application/views/admin/projects/project_staff.php
+++ application/views/admin/projects/project_staff.php
@@ -1,13 +1,13 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 $table_data = array(
     '<span class="hide"> - </span><div class="checkbox mass_select_all_wrap"><input type="checkbox" id="mass_select_all" data-to-table="staff"><label></label></div>',
-    'Vorname',
-    'Nachname',
-    'Rolle',
-    'Email',
-    'Telefonnummer',
-    _l('staff_dt_last_Login'),
-    _l('staff_dt_active'),
+    get_transl_field('tsl_staff', 'vorname','Vorname'),
+    get_transl_field('tsl_staff', 'nachname','Nachname'),
+    get_transl_field('tsl_staff', 'rolle','Rolle'),
+    get_transl_field('tsl_staff', 'email','Email'),
+    get_transl_field('tsl_staff', 'telefonnummer','Telefonnummer'),
+    get_transl_field('tsl_staff', 'letztes_login','Letztes Login'),
+    get_transl_field('tsl_staff', 'aktiv','Aktiv'),
 );
 
 $custom_fields = get_custom_fields('staff', array('show_on_table' => 1));
